05ff5c31ec2d0691e8cb527e243332a768ce7420
drkmaster1412 <98210345+drkmaster1412@users.noreply.github.com>
2022-03-14 15:32:05 +0700

    edit image

--- src/Form/ProductsType.php
+++ src/Form/ProductsType.php
@@ -6,6 +6,7 @@ use App\Entity\Products;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProductsType extends AbstractType
 {
