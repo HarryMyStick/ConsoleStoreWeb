fc8a6e5753082128b02d5dd6aa4ffe18b63b7699
drkmaster1412 <98210345+drkmaster1412@users.noreply.github.com>
2022-03-14 15:14:01 +0700

    vich enable

--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -6,9 +6,14 @@ use App\Repository\ProductsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+
+
 
 /**
  * @ORM\Entity(repositoryClass=ProductsRepository::class)
+ * @Vich\Uploadable
  */
 class Products
 {
