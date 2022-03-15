5919d8517a2c7e313aa92f71c657c95a9277e674
tt110902 <100849222+tt110902@users.noreply.github.com>
2022-03-15 15:18:26 +0700

    fix homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductsRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'homepage')]
-    public function index(): Response
+    public function index(ProductsRepository $productsRepository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('products/index.html.twig', [
+            'products' => $productsRepository->findAll(),
+        ]);
+    }
+    
+    #[Route("/product/{id}/details", name:"product.detail")]
+    public function detail(ProductController $product):Response
+    {
+        return $this->render('product/detail.html.twig',[
+            'product' => $product,
         ]);
     }
 }
