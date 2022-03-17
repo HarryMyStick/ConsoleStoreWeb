c792c1ef2542a64c74b1e46faee29acdec6aec28
drkmaster1412 <98210345+drkmaster1412@users.noreply.github.com>
2022-03-17 15:19:36 +0700

    fix account

--- src/Entity/Accounts.php
+++ src/Entity/Accounts.php
@@ -156,4 +156,8 @@ class Accounts
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+    public function __toString()
+    {
+        return $this->Username;
+    }
 }
